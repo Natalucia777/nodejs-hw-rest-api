0b695044538fc68d255cd4429f336f9b187e8e22
Natalya Demidenko <natalucia777@ukr.net>
2023-11-06 23:42:21 +0200

    hw3-5

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+
 const app = require('./app');
 
 const { DB_HOST, PORT } = process.env;
+
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}.`)
 // });
+
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
