e6f8c38127ffb0d2863e5a9db6acffaf7e3bc801
Natalya Demidenko <natalucia777@ukr.net>
2023-12-06 00:19:20 +0200

    hw04-1

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const { mongooseError } = require("../helpers");
+const Joi = require("joi");
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true });
+
+userSchema.post("save", mongooseError);
+
+const registerSchema = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min().required(),
+  subscription: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().min().required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+module.export = {
+  User,
+  schemas,
+};
