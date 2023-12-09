84356caca774582569c905e08f1a137c749ff9fa
Natalya Demidenko <natalucia777@ukr.net>
2023-12-09 13:38:37 +0200

    hw-4-9

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,21 +9,11 @@ const {
 } = require("../../middelwares");
 const { schemas } = require("../../models/contact");
 
-// router.get('/', ctrl.listContacts);
 router.get('/', authenticate, ctrl.listContacts);
-// router.get('/:id', isValidId, ctrl.getContactById);
 router.get('/:id', authenticate, isValidId, ctrl.getContactById);
-// router.delete('/:id', isValidId, ctrl.removeContact);
 router.delete('/:id', authenticate, isValidId, ctrl.removeContact);
-// router.post('/', validateBody(schemas.addContactSchema), ctrl.addNawContact);
 router.post('/', authenticate, validateBody(schemas.addContactSchema), ctrl.addNawContact);
 
-// router.put(
-//   '/:id',
-//   isValidId,
-//   validateBody(schemas.addContactSchema),
-//   ctrl.updateById
-// );
 router.put(
   '/:id',
   authenticate,
@@ -32,12 +22,6 @@ router.put(
   ctrl.updateById
 );
 
-// router.patch(
-//   '/:id/favorite',
-//   isValidId,
-//   validateFavorite(schemas.updateFavoriteSchema),
-//   ctrl.updateFavorite
-// );
 router.patch(
   '/:id/favorite',
   authenticate,
